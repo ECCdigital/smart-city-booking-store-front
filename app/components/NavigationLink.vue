057bfca801b7e57208b915e1a88d585621f22e98
NickiECC <dominique.brandt@e-c-crew.de>
2025-09-25 14:14:21 +0200

    adjusted navigation

---
+++ app/components/NavigationLink.vue
@@ -0,0 +1,37 @@
+<template>
+  <NuxtLink :to="tab.value" :class="linkClass">
+    <div :class="stripeClass"/>
+    <Icon v-if="tab.icon" :name="tab.icon" :class="iconClass" />
+    <span v-if="isActive" :class="labelClass">{{tab.label}}</span>
+
+  </NuxtLink>
+</template>
+<script setup>
+const props = defineProps({
+  tab: {
+    type: Object,
+    required: true
+  }
+})
+
+const route = useRoute()
+const isActive = computed(() => {
+return route.path=== props.tab.value
+})
+
+const linkClass= computed(() => [
+  "flex items-center px-4 relative h-12",
+    isActive.value ? "bg-[var(--color-primary)]":"bg-transparent"
+])
+const stripeClass= computed(() => [
+  "absolute top-0 left-0 w-full h-1",
+  isActive.value ? "bg-[var(--color-secondary)]/40":"bg-transparent"
+])
+const iconClass= computed(() => [
+    "text-white text-lg mr-2"
+])
+const labelClass = computed(() => [
+    "text-white text-base"
+])
+</script>
+<style scoped></style>
--- app/pages/catalog/[catalogSlug].vue
+++ app/pages/catalog/[catalogSlug].vue
@@ -3,6 +3,7 @@ import { useCatalogStore } from "~~/stores/catalog.js";
 import { useBookableStore } from "~~/stores/bookable.js";
 import { useEventStore } from "~~/stores/event.js";
 import { useCatalog } from "~/composables/api/useCatalog.js";
+import NavigationBar from "../../components/NavigationBar.vue";
 
 definePageMeta({
   layout: "catalog",
@@ -21,14 +22,7 @@ const catalogSlug = computed(() => {
   return route?.params?.catalogSlug;
 });
 
-const isValidCatalogSlug = (slug) => {
-  const invalidSlugs = ["sw.js", "robots.txt", "favicon.ico", "manifest.json"];
-
-  return !invalidSlugs.includes(slug) && !slug.includes(".");
-};
-
 const catalogStore = useCatalogStore();
-
 const catalog = computed(() => {
   return catalogStore.getCatalog;
 });
@@ -57,61 +51,9 @@ if (data.value?.bookables) {
   bookableStore.$patch({ bookables: data.value.bookables });
 }
 
-const tabs = computed(() => [
-  {
-    label: "Events",
-    icon: "i-heroicons-calendar",
-    value: `/catalog/${catalogSlug.value}/events`,
-    slot: "event"
-    /*ui: {
-      root: "bg-white",
-      list:"bg-white",
-      trigger: "bg-red-500"
-    }*/
-  },
-  {
-    label: "Test",
-    icon: "i-heroicons-beaker",
-    value: `/catalog/${catalogSlug.value}/test`,
-    slot: "test"
-    /*ui: {
-      root: "bg-white",
-      list:"bg-white",
-      trigger: "bg-red-500"
-    }*/
-  },
-  {
-    label: "Bookables",
-    icon: "i-heroicons-ticket",
-    value: `/catalog/${catalogSlug.value}/bookables`,
-    slot: "bookables",
-    ui: {
-
-      /*root: "bg-white",
-      list:"bg-white",
-      trigger: "bg-red-500"*/
-    }
-  },
-]);
-
-const defaultTabIndex = computed(() => {
-  const path = route.path;
-  if (path.includes("/events")) return 0;
-  if (path.includes("/test")) return 1;
-  if (path.includes("/bookables")) return 2;
-  return 0;
-});
 
 let isFetching = false;
 
-const active = computed({
-  get: () => {
-    return route.name;
-  },
-  set: (value) => {
-    router.push(value);
-  },
-});
 
 useHead({
   link: [
@@ -121,60 +63,21 @@ useHead({
     },
   ],
 });
-const activeTabLabel = ref("")
 </script>
 
 <template>
   <div >
-    <div class="mb-4">
-      <UTabs
-          :items="tabs"
-          :content="false"
-          :default-value="defaultTabIndex"
-          variant="pill"
-          size="lg"
-          :ui="{
-      trigger: 'text-sm font-medium rounded-none transition data-[state=active]:rounded-none data-[state=active]:bg-blue-500 hover:data-[state=inactive]:not-disabled:text-default',
-    }"
-          @update:modelValue="args => {router.push(args); activeTabLabel=args}"
-      >
-        <template #default="{ item }">
-            <span></span>
-            <span v-if="activeTabLabel === item.value">{{item.label}}</span>
-        </template>
-        <template #list-trailing>
-          <div style="flex:1"></div>
-          <UButton
-              label="Login"
-              color="primary"
-              variant="ghost"
-              @click="console.log('want login')"
-          />
-        </template>
-      </UTabs>
-      <UTabs
-        :items="tabs"
-        :content="false"
-        :default-value="defaultTabIndex"
-        variant="link"
-        size="xl"
-        class="my-tabs"
-        style="background-color: var(--color-secondary)"
-        @update:modelValue="args => {router.push(args); activeTabLabel=args}"
-      >
-        <template #default="{ item }">
-          <div :style="activeTabLabel === item.value ? 'background-color:pink' : ''">
-          <span></span>
-          <span v-if="activeTabLabel === item.value">{{item.label}}</span>
-          </div>
-        </template>
-        <template #list-trailing>
-          <div style="flex:1"></div>
-          <UButton
-              label="Login"
-              color="primary"
-              variant="ghost"
-              @click="console.log('want login')"
+    <NavigationBar/>
+
+    <UPageHero
+        title="Unsere Angebote und Veranstaltungen"
+        :ui="{
+          container: 'bg-[url(/assets/kiel_bootshafen.jpg)] contrast-70 p-10 pb-25 shadow-lg',
+          title: 'text-2xl text-black',
+        }"
+    >
+      <template #headline>
+        <div class="flex justify-center ">
           />
 
         </template>
@@ -189,16 +92,4 @@ const activeTabLabel = ref("")
 </template>
 
 <style scoped>
-/* Beispiel: Aktiver Tab */
-.my-tabs .ui-tab-selected {
-  background-color: hotpink
-  /*@apply text-blue-600 border-b-2 border-blue-600 font-semibold;*/
-}
-
-/* Beispiel: Nicht aktiver Tab */
-.my-tabs .ui-tab {
-  background-color: lightblue;
-  /*@apply text-gray-500 hover:text-gray-700 transition;*/
-}
-
 </style>
