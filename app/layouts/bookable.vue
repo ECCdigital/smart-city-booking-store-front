fb0eaaebdfd2ded50702c5a8bc9811577ae75284
NickiECC <dominique.brandt@e-c-crew.de>
2025-12-09 14:06:09 +0100

    added hero to layout

--- app/layouts/bookable.vue
+++ app/layouts/bookable.vue
@@ -12,8 +12,33 @@ useHead({
 </script>
 
 <template>
-  <div class="bg-gray-200">
+  <div>
     <NavigationBar />
+
+    <!-- toDo - adjust header for details -->
+    <div
+        class="bg-gray-200  px-10 py-15 justify-between shadow-sm hidden md:block"
+    >
+      <div class="flex">
+        <div class="grid content-center max-w-220px">
+          <p class="text-primary font-bold">Marktplatz</p>
+          <p class="text-black text-3xl font-bold">
+            Unsere Angebote und Veranstaltungen
+          </p>
+        </div>
+        <div style="flex: 1; min-width: 15vw"/>
+        <div>
+          <img
+              src="../assets/logo-kielregion.png"
+              alt="Logo Kiel Region"
+              class="text-center"
+              style="height: 7vw"
+          >
+        </div>
+      </div>
+
+    </div>
+
     <NuxtPage />
   </div>
 </template>
