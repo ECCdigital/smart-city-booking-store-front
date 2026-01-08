7b5a1a19cc1d4c8df4339c9485de00f970b8c6ae
NickiECC <dominique.brandt@e-c-crew.de>
2026-01-08 10:53:56 +0100

    Improve image area styling in DetailsAreaImages.vue

--- app/components/search/DetailsAreaImages.vue
+++ app/components/search/DetailsAreaImages.vue
@@ -1,24 +1,30 @@
 <template>
-  <div class="-mt-10" style="max-height: 350px; height: 300px; overflow: hidden;">
-    <img
-        v-if="!props.isEvent && item?.imgUrl"
-        :src="`/api/img?url=${encodeURIComponent(item.imgUrl)}`"
-        alt=""
-        class="w-full h-full object-contain rounded-l-xl"
+  <div class="flex justify-center">
+    <div
+        class="-mt-10 bg-gray-300/30 backdrop-blur-lg shadow-lg"
+        style="max-height: 450px; height: 400px; width: 70vw;overflow: hidden;"
     >
-    <img
-        v-else-if="props.isEvent && item?.information?.teaserImage"
-        :src="`/api/img?url=${encodeURIComponent(item.information.teaserImage)}`"
-        alt=""
-        class="w-full h-full object-contain rounded-l-xl"
-    >
-    <img
-        v-else
-        src="../../assets/bookable-default.jpg"
-        alt="Platzhalterbild: graue Dreiecke, keine spezifische Darstellung des Buchungsobjekts"
-        class="h-full w-full object-contain rounded-l-xl"
-    >
+      <img
+          v-if="!props.isEvent && item?.imgUrl"
+          :src="`/api/img?url=${encodeURIComponent(item.imgUrl)}`"
+          alt=""
+          class="w-full h-full object-contain rounded-l-xl"
+      >
+      <img
+          v-else-if="props.isEvent && item?.information?.teaserImage"
+          :src="`/api/img?url=${encodeURIComponent(item.information.teaserImage)}`"
+          alt=""
+          class="w-full h-full object-contain rounded-l-xl"
+      >
+      <img
+          v-else
+          src="../../assets/bookable-default.jpg"
+          alt="Platzhalterbild: graue Dreiecke, keine spezifische Darstellung des Buchungsobjekts"
+          class="h-full w-full object-contain rounded-l-xl"
+      >
+    </div>
   </div>
+
 </template>
 <script setup>
 const props = defineProps({
