3c1bcfd83bba3da4e62fcd6ac4a6291a9aa564de
Marvin Anders <marvin.anders@e-c-crew.de>
2026-01-09 16:16:29 +0100

    added detail pages to tenant context

---
+++ app/pages/t/[tenantID]/events/[eventID].vue
@@ -0,0 +1,37 @@
+<script setup>
+import { useCatalogBundle } from "~/composables/useCatalogBundle.js";
+import { useEventStore } from "~~/stores/event.js";
+import DetailsArea from "~/components/search/DetailsArea.vue";
+
+definePageMeta({
+  layout: "bookable",
+  middleware: ["catalog-auth"],
+  name: "event-id",
+});
+
+
+const route = useRoute();
+const eventStore = useEventStore();
+
+const catalogSlug = computed(() => route.params.catalogSlug);
+const eventID = computed(() => route.params.eventID);
+
+const { loadBundle } = useCatalogBundle();
+
+const event = computed(() => {
+  return eventStore.getEventById(eventID.value);
+});
+
+if (!event.value) {
+  await loadBundle({ slug: catalogSlug.value, eventID: eventID.value });
+}
+</script>
+
+<template>
+  <div v-if="event">
+    <DetailsArea :item="event" is-event/>
+  </div>
+  <p v-else>Event nicht gefunden.</p>
+</template>
+
+<style scoped></style>
---
+++ app/pages/t/[tenantID]/locations/[locationID].vue
@@ -0,0 +1,36 @@
+<script setup>
+import { useBookableStore } from "~~/stores/bookable.js";
+import { useCatalogBundle } from "~/composables/useCatalogBundle.js";
+import DetailsArea from "~/components/search/DetailsArea.vue";
+
+definePageMeta({
+  layout: "bookable",
+  middleware: ["catalog-auth"],
+  name: "location-id",
+});
+
+const route = useRoute();
+const bookableStore = useBookableStore();
+
+const catalogSlug = computed(() => route.params.catalogSlug);
+const bookableID = computed(() => route.params.locationID);
+
+const { loadBundle } = useCatalogBundle();
+
+const bookable = computed(() => {
+  return bookableStore.getBookableById(bookableID.value);
+});
+
+if (!bookable.value) {
+  await loadBundle({ slug: catalogSlug.value, bookableID: bookableID.value });
+}
+</script>
+
+<template>
+  <div v-if="bookable">
+    <DetailsArea :item="bookable" />
+  </div>
+  <p v-else>Ort nicht gefunden.</p>
+</template>
+
+<style scoped></style>
