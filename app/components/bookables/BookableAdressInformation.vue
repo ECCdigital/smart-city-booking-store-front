e02eee15d54ec217c9c06ef67595aafa1c98cf1c
Marvin Anders <marvin.anders@e-c-crew.de>
2026-01-09 15:57:19 +0100

    added neu location format

--- app/components/bookables/BookableAdressInformation.vue
+++ app/components/bookables/BookableAdressInformation.vue
@@ -2,12 +2,12 @@
 <div>
   <p>
     <UIcon name="i-lucide-map-pin" class="size-5" />
-    <span v-if="props.bookable?.location" class="p-3">{{
-        bookable.location
+    <span v-if="location.length" class="p-3">{{
+        location
       }}</span>
     <span v-else class="italic p-3">Keine Adresse bekannt.</span>
   </p>
-  <p v-if="props.bookable?.location">
+  <p v-if="location.length">
     <UIcon name="i-lucide-navigation" class="size-5" />
     <span class="p-3">Distance coming soon </span>
   </p>
@@ -19,6 +19,16 @@ const props = defineProps({
     type: Object,
     required: true,
   },
+});
+
+const location = computed(() => {
+  if(typeof props.bookable.location === 'string') {
+    return props.bookable.location;
+  } else if (props.bookable.location && typeof props.bookable.location === 'object') {
+    return props.bookable.location.display_address || '';
+  } else {
+    return '';
+  }
 });
 </script>
 
