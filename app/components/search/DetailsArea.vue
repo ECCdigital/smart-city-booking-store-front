8e1ea6663e4f8edef4e1cbabb262dde4cd5f1d4e
NickiECC <dominique.brandt@e-c-crew.de>
2026-01-15 09:08:02 +0100

    fixed layout

--- app/components/search/DetailsArea.vue
+++ app/components/search/DetailsArea.vue
@@ -1,35 +1,31 @@
-
 <template>
-  <div class="max-w-[1600px] px-2 md:px-[15px] pb-[50px]">
+  <div class="max-w-[1400px] px-2 md:px-[15px] pb-[50px]" style="margin: auto;">
     <!-- toDo - weitere Bilder einfügen *** -->
     <DetailsAreaImages :item="item" :is-event="props.isEvent" />
 
-
     <div class="w-full flex justify-between my-5">
       <UButton
-          label="Zurück"
-          icon="i-lucide-arrow-left"
-          class="justify-center px-5 bg-gray-300 text-black"
-          :style="{ cursor:'pointer' }"
-          @click="$router.back()"
+        label="Zurück"
+        icon="i-lucide-arrow-left"
+        class="justify-center px-5 bg-gray-300 text-black"
+        :style="{ cursor: 'pointer' }"
+        @click="$router.back()"
       />
       <UButton
-          label="Teilen"
-          icon="i-lucide-share-2"
-          class="justify-center px-5"
-          :style="{color: contrastToPrimary, cursor: 'pointer'}"
-          @click="share()"
+        label="Teilen"
+        icon="i-lucide-share-2"
+        class="justify-center px-5"
+        :style="{ color: contrastToPrimary, cursor: 'pointer' }"
+        @click="share()"
       />
-
     </div>
 
-    <DetailsAreaBookableContent v-if="!isEvent" :item="props.item"/>
+    <DetailsAreaBookableContent v-if="!isEvent" :item="props.item" />
     <DetailsAreaEventContent v-else :item="props.item" />
-
   </div>
 </template>
 <script setup>
-import {useContrastColor} from "~/composables/utils/useContrastColor.js";
+import { useContrastColor } from "~/composables/utils/useContrastColor.js";
 import DetailsAreaBookableContent from "~/components/search/DetailsAreaBookableContent.vue";
 import DetailsAreaImages from "~/components/search/DetailsAreaImages.vue";
 import DetailsAreaEventContent from "~/components/search/DetailsAreaEventContent.vue";
@@ -43,21 +39,19 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
-})
+});
 
 const contrastToPrimary = computed(() =>
-    useContrastColor().contrastToPrimary()
+  useContrastColor().contrastToPrimary(),
 );
 
 const notification = useNotification();
 async function share() {
   await navigator.clipboard.writeText(window.location.href);
   notification.success(
-      "Der Link zur aktuellen Suche wurde in Ihre Zwischenablage kopiert.",
-      "Link erfolgreich kopiert!"
+    "Der Link zur aktuellen Suche wurde in Ihre Zwischenablage kopiert.",
+    "Link erfolgreich kopiert!",
   );
 }
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
