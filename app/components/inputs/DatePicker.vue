e814ddab98bca207ba587ce7f34611a1f5e30ce0
Marvin Anders <marvin.anders@e-c-crew.de>
2026-01-20 14:52:14 +0100

    fix

--- app/components/inputs/DatePicker.vue
+++ app/components/inputs/DatePicker.vue
@@ -24,6 +24,7 @@
 </template>
 <script setup>
 import VueDatePicker from "@vuepic/vue-datepicker";
+import '@vuepic/vue-datepicker/dist/main.css'
 
 const model = defineModel();
 const mode = useColorMode()
