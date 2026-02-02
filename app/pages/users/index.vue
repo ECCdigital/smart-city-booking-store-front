c290c227184b71f4b422d3a012de8ba8c5cf79d0
NickiECC <dominique.brandt@e-c-crew.de>
2026-02-02 14:56:11 +0100

    Embed bookings into users index and add icons

--- app/pages/users/index.vue
+++ app/pages/users/index.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="md:container bg-neutral-50 dark:bg-gray-950 md:flex h-[60vh] pt-7">
-    <div class="border border-gray-200 rounded p-2 space-y-1 hidden md:block" style="width: 350px; max-width: 30vw">
+    <div class="md:basis-1/6 border border-gray-200 rounded p-2 space-y-1 hidden md:block">
       <div
         v-for="(item, i) in userNavigation"
         :key="i"
-        class="bg-gray-100 dark:bg-gray-900 border-b-gray-200 rounded p-2 cursor-pointer hover:bg-gray-200 hover:dark:bg-gray-800"
+        class="bg-gray-100 dark:bg-gray-900 flex rounded p-2 cursor-pointer hover:bg-gray-200 hover:dark:bg-gray-800"
         :class="activeItem === item.value ? 'font-bold bg-primary/40' : ''"
         @click="setActiveItem(item.value)"
       >
+        <UIcon :name="item.icon" class="mr-3 mt-1" />
         {{ item.label }}
       </div>
     </div>
     <UTabs v-model="activeItem" color="primary" variant="pill" :content="false" :items="userNavigation" class="mx-2 mb-5 block md:hidden" />
 
 
-    <div class="px-5">
-      <div v-if="activeItem === 'bookings'" id="bookingsContent">
+    <div class="px-5 md:basis-5/6">
+      <div v-if="activeItem === 'bookings'">
         <h2 class="text-2xl font-bold">Ihre Buchungen</h2>
+        {{ user }}
+        <hr>
+        {{allBookings}}
+      </div>
+      <div v-if="activeItem === 'invoices'">
+        <h2 class="text-2xl font-bold">Ihre Rechnungen</h2>
         {{ user }}
       </div>
 
@@ -43,14 +50,17 @@ const userNavigation = [
   {
     value: "bookings",
     label: "Buchungen",
+    icon: "i-lucide-book-marked"
   },
   {
     value: "invoices",
     label: "Rechnungen",
+    icon: "i-lucide-wallet-cards"
   },
   {
     value: "favorites",
     label: "Favoriten",
+    icon: "i-lucide-book-heart"
   },
 ];
 const activeItem = ref("bookings");
