8a0d6bcbca190ec8a613dc1752464fd95b9402bd
NickiECC <dominique.brandt@e-c-crew.de>
2026-02-18 11:21:16 +0100

    clean up

--- app/components/bookables/BookableSection.vue
+++ app/components/bookables/BookableSection.vue
@@ -2,30 +2,30 @@
   <div class="bg-neutral-50 dark:bg-gray-950">
     <div class="flex justify-center">
       <SearchBar
-          v-model:is-initailized="searchIsInitialized"
-          v-model:filter-reset-key="filterResetKey"
-          search-type="bookables"
-          :term="query.term"
-          :location="query.location"
-          :time-start="query.start"
-          :time-end="query.end"
-          @search="runSearch"
-          @reset="resetResults"
+        v-model:is-initailized="searchIsInitialized"
+        v-model:filter-reset-key="filterResetKey"
+        search-type="bookables"
+        :term="query.term"
+        :location="query.location"
+        :time-start="query.start"
+        :time-end="query.end"
+        @search="runSearch"
+        @reset="resetResults"
       />
     </div>
 
     <div class="m-10 lg:m-5 sm:flex items-center">
       <span
-          v-if="searchIsInitialized"
-          class="text-black dark:text-white lg:font-bold"
-      >{{ suitableCount }} {{ $t("filter.fittingResults") }}</span
+        v-if="searchIsInitialized"
+        class="text-black dark:text-white lg:font-bold"
+        >{{ suitableCount }} {{ $t("filter.fittingResults") }}</span
       >
       <div class="" style="flex: 1" />
       <div class="flex space-x-2 mt-2 sm:mt-0 -ml-2 sm:ml-0">
         <SortButton
-            v-if="searchedResources.length > 0"
-            :sort-mode="query.sortMode"
-            @sort="setSortedQueryParams"
+          v-if="searchedResources.length > 0"
+          :sort-mode="query.sortMode"
+          @sort="setSortedQueryParams"
         />
         <FilterButton
           v-if="searchedResources.length > 0"
@@ -46,17 +46,17 @@
     <div class="flex flex-row lg:my-5 m-5">
       <div class="lg:basis-1/4 hidden lg:block">
         <FilterArea
-            v-if="searchedResources.length > 0"
-            :key="filterResetKey"
-            v-model:is-initailized="searchIsInitialized"
-            :include-non-suitable="query.inclNoSuitable"
-            :categories="query.cat"
-            :cities="query.cities"
-            :price="query.price"
-            :only-public-events="query.pubEv"
-            :only-registered-events="query.regEv"
-            :bookables="searchedResources"
-            @filter="setFilterQueryParams"
+          v-if="searchedResources.length > 0"
+          :key="filterResetKey"
+          v-model:is-initailized="searchIsInitialized"
+          :include-non-suitable="query.inclNoSuitable"
+          :categories="query.cat"
+          :cities="query.cities"
+          :price="query.price"
+          :only-public-events="query.pubEv"
+          :only-registration-needed-events="query.regEv"
+          :bookables="searchedResources"
+          @filter="setFilterQueryParams"
         />
       </div>
 
@@ -70,18 +70,18 @@
           <p class="text-gray-500">{{ $t("resources.noResources") }}</p>
         </div>
         <ResultsList
-            v-if="sortedResources.length > 0"
-            :bookables="sortedResources"
-            include-non-bookable
-            include-non-suitable
-            class="hidden md:block"
+          v-if="sortedResources.length > 0"
+          :bookables="sortedResources"
+          include-non-bookable
+          include-non-suitable
+          class="hidden md:block"
         />
         <ResultsGrid
-            v-if="sortedResources.length > 0"
-            :bookables="sortedResources"
-            include-non-bookable
-            include-non-suitable
-            class="md:hidden"
+          v-if="sortedResources.length > 0"
+          :bookables="sortedResources"
+          include-non-bookable
+          include-non-suitable
+          class="md:hidden"
         />
       </div>
     </div>
@@ -94,7 +94,7 @@ import ResultsGrid from "~/components/search/ResultsGrid.vue";
 import ResultsList from "~/components/search/ResultsList.vue";
 import SearchBar from "~/components/search/SearchBar.vue";
 import SortButton from "~/components/search/SortButton.vue";
-import {useBookableSearch} from "~/composables/search/useBookableSearch.js";
+import { useBookableSearch } from "~/composables/search/useBookableSearch.js";
 
 const props = defineProps({
   bookables: {
--- app/components/events/EventSection.vue
+++ app/components/events/EventSection.vue
@@ -2,31 +2,31 @@
   <div class="bg-neutral-50 dark:bg-gray-950">
     <div class="flex justify-center">
       <SearchBar
-          v-model:is-initailized="searchIsInitialized"
-          v-model:filter-reset-key="filterResetKey"
-          search-type="events"
-          :term="query.term"
-          :location="query.location"
-          :time-start="query.start"
-          :time-end="query.end"
-          @search="runSearch"
-          @reset="resetResults"
+        v-model:is-initailized="searchIsInitialized"
+        v-model:filter-reset-key="filterResetKey"
+        search-type="events"
+        :term="query.term"
+        :location="query.location"
+        :time-start="query.start"
+        :time-end="query.end"
+        @search="runSearch"
+        @reset="resetResults"
       />
     </div>
 
     <div class="m-10 lg:m-5 sm:flex items-center">
       <span
-          v-if="searchIsInitialized"
-          class="text-black dark:text-white lg:font-bold"
-      >{{ suitableCount }}  {{ $t("filter.fittingResults") }}</span
+        v-if="searchIsInitialized"
+        class="text-black dark:text-white lg:font-bold"
+        >{{ suitableCount }} {{ $t("filter.fittingResults") }}</span
       >
       <div class="" style="flex: 1" />
       <div class="flex space-x-2 mt-2 sm:mt-0 -ml-2 sm:ml-0">
         <SortButton
-            v-if="searchedEvents.length > 0"
-            :sort-mode="query.sortMode"
-            is-event
-            @sort="setSortedQueryParams"
+          v-if="searchedEvents.length > 0"
+          :sort-mode="query.sortMode"
+          is-event
+          @sort="setSortedQueryParams"
         />
         <FilterButton
           v-if="searchedEvents.length > 0"
@@ -48,17 +48,18 @@
     <div class="flex flex-row lg:my-5 m-5">
       <div class="lg:basis-1/4 hidden lg:block">
         <FilterArea
-            v-if="searchedEvents.length > 0"
-            :key="filterResetKey"
-            v-model:is-initailized="searchIsInitialized"
-            :include-non-suitable="query.inclNoSuitable"
-            :cities="query.cities"
-            :price="query.price"
-            is-event
-            :only-public-events="query.pubEv"
-            :only-registered-events="query.regEv"
-            :bookables="searchedEvents"
-            @filter="setFilterQueryParams"
+          v-if="searchedEvents.length > 0"
+          :key="filterResetKey"
+          v-model:is-initailized="searchIsInitialized"
+          :include-non-suitable="query.inclNoSuitable"
+          :cities="query.cities"
+          :price="query.price"
+          :categories="query.cat"
+          is-event
+          :only-public-events="query.pubEv"
+          :only-registration-needed-events="query.regEv"
+          :bookables="searchedEvents"
+          @filter="setFilterQueryParams"
         />
       </div>
 
@@ -72,20 +73,20 @@
           <p class="text-gray-500">{{ $t("events.noEvents") }}</p>
         </div>
         <ResultsList
-            v-if="sortedEvents.length > 0"
-            :bookables="sortedEvents"
-            include-non-bookable
-            include-non-suitable
-            is-event-list
-            class="hidden md:block"
+          v-if="sortedEvents.length > 0"
+          :bookables="sortedEvents"
+          include-non-bookable
+          include-non-suitable
+          is-event-list
+          class="hidden md:block"
         />
         <ResultsGrid
-            v-if="sortedEvents.length > 0"
-            :bookables="sortedEvents"
-            include-non-bookable
-            include-non-suitable
-            is-event-grid
-            class="md:hidden"
+          v-if="sortedEvents.length > 0"
+          :bookables="sortedEvents"
+          include-non-bookable
+          include-non-suitable
+          is-event-grid
+          class="md:hidden"
         />
       </div>
     </div>
@@ -98,7 +99,7 @@ import ResultsGrid from "~/components/search/ResultsGrid.vue";
 import ResultsList from "~/components/search/ResultsList.vue";
 import SearchBar from "~/components/search/SearchBar.vue";
 import SortButton from "~/components/search/SortButton.vue";
-import {useBookableSearch} from "~/composables/search/useBookableSearch.js";
+import { useBookableSearch } from "~/composables/search/useBookableSearch.js";
 
 const props = defineProps({
   events: {
@@ -119,10 +120,6 @@ const {
   runSearch,
   resetResults,
 } = useBookableSearch({ isEvent: true, sourceItems: props.events });
-
 </script>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
