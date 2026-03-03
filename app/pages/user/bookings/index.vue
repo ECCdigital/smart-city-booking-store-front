a704bf0b5c9732653dda24e09299d92bc8d28083
NickiECC <dominique.brandt@e-c-crew.de>
2026-03-03 11:02:18 +0100

    Extract booking search/filter to new component

--- app/pages/user/bookings/index.vue
+++ app/pages/user/bookings/index.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="w-full">
-    <PageHeader title="Ihre Buchungen" />
+    <div class="md:flex justify-between items-center w-full mb-4">
+      <PageHeader title="Ihre Buchungen" class="mb-3 md:mb-0"/>
+      <BookingSearchFilterArea
+        :bookings="bookings"
+        @update:bookings="setFilteredBookings"
+      />
+    </div>
+
     <BookingsSkeleton v-if="pending" :skeleton-count="9" />
-    <BookingSection v-else-if="bookings?.length" :bookings="bookings" />
+    <BookingSection v-else-if="filteredBookings?.length" :bookings="filteredBookings" />
     <BookingEmptyState v-else />
   </div>
 </template>
@@ -11,6 +18,7 @@ import BookingSection from "~/components/user/BookingSection.vue";
 import { useBookingStore } from "~~/stores/bookings.js";
 import BookingsSkeleton from "~/components/user/bookings/BookingsSkeleton.vue";
 import BookingEmptyState from "~/components/user/bookings/BookingEmptyState.vue";
+import BookingSearchFilterArea from "~/components/user/bookings/BookingSearchFilterArea.vue";
 
 definePageMeta({
   name: "bookings",
@@ -21,10 +29,15 @@ const bookingsStore = useBookingStore();
 await bookingsStore.fetchBookings();
 
 const { pending } = useAsyncData("bookings", () =>
-  bookingsStore.fetchBookings()
+  bookingsStore.fetchBookings(),
 );
 
 const bookings = computed(() => bookingsStore.getBookings);
+const filteredBookings = ref(bookings.value.sort((a, b) => b.timeCreated - a.timeCreated));
+
+function setFilteredBookings(bookings) {
+  filteredBookings.value = bookings.map(b => ({ ...b }));
+}
 </script>
 
 <style scoped></style>
