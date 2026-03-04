1f5f38186ff0898873c4b04d51389e8b5e0ac174
NickiECC <dominique.brandt@e-c-crew.de>
2026-03-04 12:59:07 +0100

    Add BookableRelatedItems and integrate in details

---
+++ app/components/bookables/BookableRelatedItems.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+      class="py-2 space-y-1"
+  >
+    <!-- toDo - change to -> v-for="bookable in item.relatedBookables" -->
+    <div
+        v-for="item in _relatedBookables"
+        :key="item.id"
+        class="flex justify-between items-center bg-white dark:bg-gray-700 rounded-lg shadow-sm px-4 py-2 border border-gray-200 dark:border-gray-500 min-w-[250px]"
+    >
+      <div class="space-y-0.5">
+        <BookableTypeBadge
+            :type="item.type"
+            :ui="{
+                  base: 'bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-gray-300',
+                }"
+        />
+        <div
+            class="text-sm font-semibold text-gray-700 dark:text-gray-200"
+        >
+          {{ item.title }}
+        </div>
+      </div>
+      <UButton
+          class="text-sm bg-primary text-white px-3 py-1"
+          label="Ansehen"
+          @click="goToDetails(item.id, item.type)"
+      />
+    </div>
+  </div>
+</template>
+<script setup>
+import BookableTypeBadge from "~/components/bookables/BookableTypeBadge.vue";
+import {useRedirection} from "~/composables/utils/useRedirection.js";
+
+const props = defineProps({
+  relatedBookables: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const {goToDetails} = useRedirection()
+
+const _relatedBookables = computed( () => {
+  if(props.relatedBookables.length === 0) {
+    return testRelatedBookables
+  }
+  return props.relatedBookables
+})
+
+const testRelatedBookables = [
+  { id: 123, title: "Beamer", type: "resource" },
+  { id: 456, title: "Toiletten", type: "room" },
+  { id: 154, title: "Parkplatz für extra große Angeber-Autos", type: "resource" },
+  { id: 789, title: "Kaffeeküche", type: "event-location" },
+  { id: 321, title: "Reinigung", type: "ticket" },
+  { id: 721, title: "Einführungskurs", type: "event" },
+];
+</script>
+
+<style scoped></style>
