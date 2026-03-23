c72d188628e15d1de4c015a882814c3e82abb084
NickiECC <dominique.brandt@e-c-crew.de>
2026-03-23 08:22:04 +0100

    Exclude service fees; handle missing holidays

--- app/composables/search/useBookableSearch.ts
+++ app/composables/search/useBookableSearch.ts
@@ -180,9 +180,14 @@ export function useBookableSearch<TItem extends { isBookable: boolean }>(
       return -1;
     }
 
+      //exclude external service fees
+      const pricesWithoutServiceFees = bookable.item.priceCategories.filter(
+          (c) => !c.external || (c.external && c.unit !== "service-fee"),
+      );
+
     //exclude holiday price categories
-    const pricesWithoutHolidays = bookable.item.priceCategories.filter(
-      (c) => c.holidays.length === 0,
+    const pricesWithoutHolidays = pricesWithoutServiceFees.filter(
+      (c) => !c.holidays || c.holidays.length === 0,
     );
 
     const minPrice = Math.min(...pricesWithoutHolidays.map((c) => c.priceEur));
