5f6e9d0d23705777ff291e2841a2659bca369807
Marvin Anders <marvin.anders@e-c-crew.de>
2026-04-13 09:25:50 +0200

    adjusted routes

--- server/api/bookings/ical.get.js
+++ server/api/bookings/ical.get.js
@@ -3,18 +3,16 @@ import { serverFetch } from "~~/server/api/utils/serverFetch.ts";
 export default defineEventHandler(async (event) => {
   const tenantID = getQuery(event).tenantId;
   const bookingId = getQuery(event).bookingId;
-  console.log("Received request for iCal download with bookingId:", bookingId);
 
   const { data, error } = await serverFetch(
     event,
-    `/api/${tenantID}/bookings/${bookingId}/ical`,
+    `/api/${tenantID}/ical/bookings/${bookingId}`,
     {
       method: "GET",
     },
   );
 
   if (error) {
-    console.error("Error fetching iCal data:", error);
     throw createError({
       statusCode: error.status || 500,
       statusMessage: "Failed to fetch iCal data",
--- server/api/events/ical.get.js
+++ server/api/events/ical.get.js
@@ -3,24 +3,21 @@ import { serverFetch } from "~~/server/api/utils/serverFetch.ts";
 export default defineEventHandler(async (event) => {
   const tenantID = getQuery(event).tenantId;
   const eventId = getQuery(event).eventId;
-  console.log("Received request for iCal download with event ID:", eventId);
 
   const { data, error } = await serverFetch(
     event,
-    `/api/${tenantID}/events/${eventId}/ical`,
+    `/api/${tenantID}/ical/events/${eventId}`,
     {
       method: "GET",
     },
   );
 
   if (error) {
-    console.error("Error fetching iCal data:", error);
     throw createError({
       statusCode: error.status || 500,
       statusMessage: "Failed to fetch iCal data",
       data: error.message,
     });
   }
-  console.log("*iCal data fetched successfully:", data);
   return data;
 });
