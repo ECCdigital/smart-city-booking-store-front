7f7aa41982fee4d592e893948983a72503a6d723
NickiECC <162126523+NickiECC@users.noreply.github.com>
2026-07-02 12:09:07 +0200

    Set Detail-Button as outlined

--- app/components/search/ResultStripBookableContent.vue
+++ app/components/search/ResultStripBookableContent.vue
@@ -51,7 +51,7 @@
         >
           <UButton
             label="Details ansehen"
-            :variant="entryPageMode ? 'solid' : 'ghost'"
+            :variant="entryPageMode ? 'solid' : 'outline'"
             class="justify-center px-10 text-color-dark dark:text-color-light"
             :style="{ cursor: 'pointer' }"
             @click="goToDetails(bookable.id, bookable.type)"
