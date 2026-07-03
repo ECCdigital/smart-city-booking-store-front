3e23897591cd1183e012757b1313be9c289aa0a9
NickiECC <162126523+NickiECC@users.noreply.github.com>
2026-07-03 08:52:57 +0200

    Render BackButton only when history exists

--- app/components/BackButton.vue
+++ app/components/BackButton.vue
@@ -1,17 +1,17 @@
 <template>
-  <UButton
+  <div>
+    <UButton
+      v-if="canGoBack"
       label="Zurück"
       icon="i-lucide-arrow-left"
       class="justify-center px-5 bg-gray-300 text-black"
       :style="{ cursor: 'pointer' }"
       @click="$router.back()"
-  />
+    />
+  </div>
 </template>
 <script setup>
-
+const canGoBack = computed(() => window?.history.length > 1);
 </script>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
